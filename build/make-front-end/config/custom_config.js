280d4a2cf9a89feea79423d95829af5b5fc5b0fd
ppzzhh1186861238 <ppzzhh1186861238@hotmail.com>
2022-03-12 17:57:25 +0800

    fix bug, add Links, update Performance.

--- build/make-front-end/config/custom_config.js
+++ build/make-front-end/config/custom_config.js
@@ -8,7 +8,7 @@ const sourceUrls =
 
 const queryPort = "8001";
 const queryAPI = "https://apihk.asdb.live/Api/Subtitle_Search/sub_search";
-const urlChoice = 1;
+const urlChoice = 0;
 const APIHost = "https://apihk.asdb.live/Api/List_Json/V1";// https://github.com/A-Soul-Database/API/tree/master/Main-Api 的API，用于获取json文件
 export const config =  {
     RunOnLocal:urlChoice !== 3,//当urlChoice不为0时，说明工程在本地跑
