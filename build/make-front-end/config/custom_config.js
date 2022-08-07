5fe630301385c4a0614a77bd2c6aa52b5d321eed
PPPPP <ppzzhh1186861238@hotmail.com>
2022-08-07 12:03:00 +0800

    back

--- build/make-front-end/config/custom_config.js
+++ build/make-front-end/config/custom_config.js
@@ -5,9 +5,9 @@ const sourceUrls =
     ]
 
 const queryPort = "8001";
-const queryAPI = "http://47.93.157.77/Api/Subtitle_Search/sub_search";
+const queryAPI = "https://apihk.asdb.live/Api/Subtitle_Search/sub_search";
 const urlChoice = 0;
-const APIHost = "http://47.93.157.77/Api/List_Json/V1";// https://github.com/A-Soul-Database/API/tree/master/Main-Api 的API，用于获取json文件
+const APIHost = "https://apihk.asdb.live/Api/List_Json/V1";// https://github.com/A-Soul-Database/API/tree/master/Main-Api 的API，用于获取json文件
 export const config =  {
     RunOnLocal:urlChoice !== 0,//当urlChoice不为0时，说明工程在本地跑
     sourceUrl:sourceUrls[urlChoice],
